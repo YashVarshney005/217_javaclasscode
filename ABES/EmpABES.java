f883ccdd4243f73a7448ea7c428b544ad9ec1ae9
YashVarshney005 <dr.infinity000@gmail.com>
2025-08-26 14:25:35 +0530

    Update EmpABES.java

--- ABES/EmpABES.java
+++ ABES/EmpABES.java
@@ -1,19 +1,14 @@
-package ABES;
+package EmpABES;
 import KIET.*;
-public class EmpABES extends EmployeeKiet{
-    public static void main(String args[]){
-        // EmployeeKiet ee1 = new EmployeeKiet();
-        // ee1.Emp_name = "Bhoiiiii Yash";
-        // ee1.salary = 32553;
-        // ee1.Emp_id = 201;
-        // ee1.Organisation = "CIAKL";
-        // ee1.employeeDatails();
 
-        // EmpABES ae = new EmpABES();
-        // ae.Emp_name = "Bhoiiiii Varshney";
-        // ae.salary = 89933;
-        // ae.Emp_id = 20178;
-        // ae.Organisation = "AKL";
-        // ae.employeeDatails();
+public class EmployeeABES extends EmployeeKIET{
+        public static void main(String[] args) {
+        EmployeeABES ea = new EmployeeABES();
+        ea.Emp_Name = "Yash ABES";
+        ea.Emp_ID = 301;
+        // ea.Org = "ABES";
+        // ea.Salary = 30000;
+
+        // ea.EmployeeDetails();
     }
 }
